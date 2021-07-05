96c84cc1577e50428c0cc2c3a509aee3c9243e5d
Ross Mulcare <ross.mulcare@gmail.com>
2021-07-05 12:27:16 -0400

    Update 'Commodities' (21–21.8)

--- _build/src/pages/Rules.jsx
+++ _build/src/pages/Rules.jsx
@@ -440,23 +440,24 @@ secondary abilities of the “Politics” strategy card.</RuleText>
     </RuleSection>
 
     <RuleSection
-      number='20'
+      number='21'
       title='Commodities'
       related={['Deals', 'Trade Goods', 'Transactions']}>
       <RuleText>Commodities represent goods that are plentiful for their own faction and are desired by other factions. A commodity has no inherent game effects, but converts into a trade good if given to or received from another player.</RuleText>
-      <RuleText number='20.1'>Commodities and trade goods are represented by opposite sides of the same token.</RuleText>
-      <RuleText number='20.2'>The commodity value on a player’s faction sheet indicates the maximum number of commodities he can have.</RuleText>
-      <RuleText number='20.3'>When an effect instructs a player to replenish commodities, he takes the number of commodity tokens necessary so that the amount of commodities he has equals the commodity value on his faction sheet. Then, he places those tokens so their commodity sides are faceup in the commodity area of his faction sheet.</RuleText>
-      <RuleText number='20.4'>When a player replenishes commodities, he takes the commodity tokens from the supply.</RuleText>
-      <RuleText number='20.5'>Players can trade commodities following the rules for transactions. When a player receives a commodity from another player, the player who received that token converts it into a trade good by placing it in the trade good area of his command sheet with the trade good side faceup.
+      <RuleText number='21.1'>Commodities and trade goods are represented by opposite sides of the same token.</RuleText>
+      <RuleText number='21.2'>TThe commodity value on a player’s faction sheet indicates the maximum number of commodities that player can have.</RuleText>
+      <RuleText number='21.3'>When an effect instructs a player to replenish commodities, that player takes the number of commodity tokens necessary so that the amount of commodities that player has equals the commodity value on their faction sheet. Then, those tokens are placed faceup in the commodity area of that player’s faction sheet.</RuleText>
+      <RuleText number='21.4'>When a player replenishes commodities, that player takes the commodity tokens from the supply.</RuleText>
+      <RuleText number='21.5'>Players can trade commodities following the rules for transactions. When a player receives a commodity from another player, the player who received that token converts it into a trade good by placing it in the trade good area of their command sheet with the trade good side faceup.
         <ul>
           <li>That token is no longer a commodity token; it is a trade good token.</li>
-          <li>A player can trade commodity tokens before resolving a game effect that allows him to replenish commodities.</li>
+          <li>A player can trade commodity tokens before resolving a game effect that allows them to replenish commodities.</li>
+          <li>If a game effect instructs a player to convert a number of their own commodities to trade goods, those trade goods are not treated as being gained for the purpose of triggering other abilities.</li>
         </ul>
       </RuleText>
-      <RuleText number='20.6'>Any game effect that instructs a player to give a commodity to another player causes that commodity to be converted into a trade good.</RuleText>
-      <RuleText number='20.7'>A player cannot spend commodities; he can only trade them during a transaction.</RuleText>
-      <RuleText number='20.8'>Commodity tokens come in values of one and three. A player can swap between these tokens as necessary.</RuleText>
+      <RuleText number='21.6'>Any game effect that instructs a player to give a commodity to another player causes that commodity to be converted into a trade good.</RuleText>
+      <RuleText number='21.7'>A player cannot spend commodities unless otherwise specified; a player can only trade them during a transaction.</RuleText>
+      <RuleText number='21.8'>Commodity tokens come in values of one and three. A player can swap between these tokens as necessary.</RuleText>
     </RuleSection>
 
     <RuleSection
